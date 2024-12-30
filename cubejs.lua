f478cffb020688802774b3ce17c8bce9c5936634
tfzrch <41972466+tfzrch@users.noreply.github.com>
2024-12-30 21:44:05 +0000

    feat: Init repo with basic CubeJS snippets

---
+++ cubejs.lua
@@ -0,0 +1,53 @@
+return {
+  s({trig="cjc", name="cubejs-js-column"}, fmt([[
+    {1}: {{
+      title: "{2}",
+      sql: `{3}`,
+      type: "{4}"
+    }}
+  ]], {
+    [1] = i(1, "dimension_name"),
+    [2] = i(2, "title"),
+    [3] = i(3, "sql"),
+    [4] = i(4, "type")
+  })),
+  s({trig="cyc", name="cubejs-yml-column"}, fmt([[
+    - name: {1}
+      title: {2}
+      sql: "{3}"
+      type: {4}
+  ]], {
+    [1] = i(1, "dimension_name"),
+    [2] = i(2, "title"),
+    [3] = i(3, "sql"),
+    [4] = i(4, "type")
+  })),
+  s({trig="cube", name="cubejs-js-cube"}, fmt([[
+    cube("{1}", {{
+      data_source: "{2}",
+      public: false,
+      sql_table: `public."{3}"`,
+      sql_alias: "{4}",
+      joins: {{
+
+      }},
+      dimensions: {{
+
+      }},
+      measures: {{
+
+      }},
+      segments: {{
+
+      }},
+      pre_aggregations: {{
+
+      }}
+    }})
+  ]], {
+    [1] = i(1, "cube"),
+    [2] = i(2, "data_source"),
+    [3] = i(3, "sql_table"),
+    [4] = i(4, "sql_alias")
+  }))
+}
