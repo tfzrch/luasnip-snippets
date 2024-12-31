1553cacde6c4bad83075ac22d38f096d2be0b6a4
tfzrch <41972466+tfzrch@users.noreply.github.com>
2024-12-31 10:09:04 +0000

    feat(yaml/cube): add snippet for cubes

--- yaml/cubejs.lua
+++ yaml/cubejs.lua
@@ -9,5 +9,28 @@ return {
     [2] = i(2, "title"),
     [3] = i(3, "sql"),
     [4] = i(4, "type")
+  })),
+  s({trig="cube", name="cubejs-yml-cube"}, fmt([[
+  cubes:
+    - name: {1}
+      data_source: "{2}"
+      public: false
+      sql_table: "public.\"{3}\""
+      sql_alias: {4}
+
+      joins: []
+
+      dimensions: []
+
+      measures: []
+
+      segments: []
+
+      pre_aggregations: []
+  ]], {
+    [1] = i(1, "cube"),
+    [2] = i(2, "data_source"),
+    [3] = i(3, "sql_table"),
+    [4] = i(4, "sql_alias")
   }))
 }
